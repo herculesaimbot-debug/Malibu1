1d9b38a1e97cf14868abb9f7efc4f1451f38ce3d
herculesaimbot-debug <herculesaimbot@gmail.com>
2026-02-17 14:28:30 -0300

    Add VIP Rubi preference to pricing options

--- netlify/functions/mp_create_preference.js
+++ netlify/functions/mp_create_preference.js
@@ -11,7 +11,8 @@ const PRODUCTS = [
   { id:"vip_ouro", price:33.99, title:"VIP Ouro (30 dias)" },
   { id:"vip_platina", price:38.99, title:"VIP Platina (30 dias)" },
   { id:"vip_diamante", price:48.99, title:"VIP Diamante (30 dias)" },
-
+  { id:"vip_rubi", price:48.99, title:"VIP Diamante (30 dias)" },
+  
   { id:"coins_1m", price:20.00, title:"1.000.000" },
   { id:"coins_2m", price:35.00, title:"2.000.000" },
   { id:"coins_3m", price:50.00, title:"3.000.000" },
