8aa1f5c62bf6e132731ea99f8f37eb8aa92f9d94
herculesaimbot-debug <herculesaimbot@gmail.com>
2026-02-17 12:39:01 -0300

    Change logout response to redirect with cookie removal
    Update logout handler to redirect and clear session cookie.

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -2,12 +2,20 @@ const { cookie } = require("./_session");
 
 exports.handler = async () => {
   return {
-    statusCode: 200,
-    headers: {
-      "Set-Cookie": cookie("discord_session", "", { maxAge: 0 }),
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store",
+    statusCode: 302,
+    multiValueHeaders: {
+      "Set-Cookie": [
+        cookie("discord_session", "", {
+          maxAge: 0,
+          sameSite: "Lax",
+          secure: true
+        })
+      ]
     },
-    body: JSON.stringify({ ok: true }),
+    headers: {
+      "Location": "/",
+      "Cache-Control": "no-store"
+    },
+    body: ""
   };
 };
