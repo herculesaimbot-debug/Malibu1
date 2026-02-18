faf25737d24054bd60fd07220b5f8f8b8b1d0456
herculesaimbot-debug <herculesaimbot@gmail.com>
2026-02-18 14:00:14 -0300

    Update get_products.js

--- netlify/functions/get_products.js
+++ netlify/functions/get_products.js
@@ -7,6 +7,7 @@ exports.handler = async () => {
     { id:"vip_ouro", category:"VIPs", name:"VIP Ouro (30 dias)", price:33.99, tag:"VIP", desc:"Após a compra, abra um ticket.", img:"/assets/products/ouro.webp" },
     { id:"vip_platina", category:"VIPs", name:"VIP Platina (30 dias)", price:38.99, tag:"VIP", desc:"Após a compra, abra um ticket.", img:"/assets/products/platina.webp" },
     { id:"vip_diamante", category:"VIPs", name:"VIP Diamante (30 dias)", price:48.99, tag:"VIP", desc:"Após a compra, abra um ticket.", img:"/assets/products/diamante.webp" },
+    { id:"vip_rubi", category:"VIPs", name:"VIP RUBI (30 dias)", price:00.10, tag:"VIP", desc:"Após a compra, abra um ticket.", img:"/assets/products/diamante.webp" },
     
 
     // Dinheiro
